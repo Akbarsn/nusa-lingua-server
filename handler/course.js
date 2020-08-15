25aac262525d66e6cd9bc8a27883fb58f96c5856
Akbarsn <personal.akbarsn@gmail.com>
2020-08-15 14:38:15 +0700

    Add handler for exam

--- handler/course.js
+++ handler/course.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose')
+const joi = require('joi')
 const Course = require('../model/Course')
 const User = require('../model/User')
-const { GeneralError, BadRequest } = require('../utils/errors')
-const { updateOne } = require('../model/Course')
+const Exam = require('../model/Exam')
+const { GeneralError, BadRequest, NotAcceptable } = require('../utils/errors')
+const e = require('express')
+const { valid } = require('joi')
 
 module.exports = {
     async GetCourseDetailByID(req, res, next) {
@@ -23,14 +26,14 @@ module.exports = {
 
     async GetTopicByID(req, res, next) {
         const courseID = mongoose.Types.ObjectId(req.params.courseID)
-        const topicID =  mongoose.Types.ObjectId(req.params.topicID)
-        
+        const topicID = req.params.topicID
+
         try {
             const course = await Course.findById(courseID)
-            if(course){
-                const topic = course.topics.find((element)=> element.id === topicID);
+            if (course) {
+                const topic = course.topics.find((e) => e.id == topicID);
 
-                if(typeof topic !== 'undefined'){
+                if (typeof topic !== 'undefined') {
                     res.status(200).json(topic)
                 } else {
                     throw new BadRequest("Topic not found")
@@ -52,16 +55,16 @@ module.exports = {
             const user = await User.findById(userID)
 
             const courses = user.courses
-            if(courses.find((e)=> e.title !== course.title)){
+            if (!courses.find((e) => e.title === course.title)) {
                 courses.push({
+                    courseID: courseID,
                     title: course.title,
-                    progress: 0
                 })
             }
 
             const update = await User.updateOne({ _id: userID }, { courses: courses })
 
-            if(update.ok !== 0){
+            if (update.ok !== 0) {
                 const updatedCourse = await Course.updateOne({ _id: courseID }, { userJoined: course.userJoined + 1 })
 
                 if (updatedCourse) {
@@ -79,23 +82,33 @@ module.exports = {
 
     async NextTopic(req, res, next) {
         const courseID = mongoose.Types.ObjectId(req.params.courseID)
-        const topicID = mongoose.Types.ObjectId(req.params.topicID)
+        const topicID = req.params.topicID
         const userID = mongoose.Types.ObjectId(req.user.id)
 
         try {
             const course = await Course.findById(courseID)
 
-            if(course){
-                const userCourse = user.courses.forEach(e => {
-                    if(e.title === course.title){   
-                        e.progress = Math.round(topicID / course.topics.length) 
-                    }
-                });
-                
-                const user = await updateOne({_id: userID}, {courses: userCourse})
-            
-                const topic = course.topics.find((e)=> e.id === topicID)
-                res.status(200).json(topic)
+            if (course) {
+                const user = await User.findById(userID)
+                if (user) {
+                    let updatedCourses = []
+                    user.courses.forEach(e => {
+                        if (e.title === course.title) {
+                            if (e.progress <= Math.round(topicID * 100 / course.topics.length)) {
+                                e.progress = Math.round(topicID * 100 / course.topics.length)
+                            }
+                        }
+
+                        updatedCourses.push(e)
+                    });
+
+                    await User.updateOne({ _id: userID }, { courses: updatedCourses })
+
+                    const topic = course.topics.find((e) => e.id == topicID)
+                    res.status(200).json(topic)
+                } else {
+                    throw new GeneralError("User not found")
+                }
             } else {
                 throw new BadRequest("Course not found")
             }
@@ -105,10 +118,90 @@ module.exports = {
     },
 
     async TakeExam(req, res, next) {
+        const courseID = req.params.courseID
+        const userID = mongoose.Types.ObjectId(req.user.id)
 
+        try {
+            const user = await User.findById(userID)
+            const course = user.courses.find((e) => `${e.courseID}` == courseID)
+
+            if (course.progress >= 100) {
+                const exam = await Exam.findOne({ courseID: course.courseID })
+
+                if (exam) {
+                    res.status(200).json(exam.questions)
+                } else {
+                    throw new GeneralError("Question not found")
+                }
+            } else {
+                throw new NotAcceptable("Courses not yet finished")
+            }
+        } catch (error) {
+            next(error)
+        }
     },
-    
-    async PostExam(req, res, next) {
 
+    async PostExam(req, res, next) {
+        const courseID = req.params.courseID
+        const userID = mongoose.Types.ObjectId(req.user.id)
+
+        try {
+            const schema = joi.array().items(joi.object({
+                id: joi.number().required(),
+                answer: joi.string().max(1).required()
+            }))
+
+            const validateReq = await schema.validateAsync(req.body)
+
+            if (validateReq.error !== null) {
+                const answers = validateReq
+                
+                const user = await User.findById(userID)
+                const exam = await Exam.findOne({ courseID: courseID })
+
+                if (exam) {
+                    const courses = user.courses.find((e) => e.courseID == courseID)
+                    if (!courses.isGraduate) {
+                        let correct = 0
+
+                        answers.forEach((answer) => {
+                            exam.answer.forEach((key) => {
+                                if (answer.id == key.id) {
+                                    if (answer.answer === key.answer) correct++;
+                                }
+                            })
+                        })
+
+                        
+                        const score = Math.round(correct * 100 / answers.length)
+                        if (score >= 80) {
+                            var updatedCourse = []
+
+                            user.courses.forEach((course) => {
+                                if (course.courseID == courseID) {
+                                    course.isGraduate = true
+                                }
+
+                                updatedCourse.push(course)
+                            })
+
+                            await User.updateOne({ _id: userID }, { courses: updatedCourse })
+                        }
+                        res.status(200).json({
+                            message: score >= 80 ? "Congrats you graduated from this course" : "So close, please try this test again",
+                            score: score
+                        })
+                    } else {
+                        throw new NotAcceptable("You already graduated from this course")
+                    }
+                } else {
+                    throw new BadRequest("Exam not found")
+                }
+            } else {
+                throw new BadRequest("Data is missing or invalid")
+            }
+        } catch (error) {
+            next(error)
+        }
     }
 }
