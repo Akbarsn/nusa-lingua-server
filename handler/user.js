ebb3a9ed1d0d645c5f1996df1e2ecd2661bf038b
Akbarsn <personal.akbarsn@gmail.com>
2020-08-15 16:17:23 +0700

    Add My Course

--- handler/user.js
+++ handler/user.js
@@ -2,16 +2,78 @@ const User = require('../model/User')
 const { BadRequest } = require('../utils/errors')
 
 module.exports = {
-    async GetProfile (req,res,next){
+    async GetProfile(req, res, next) {
+        const userID = req.user.id
+
+        console.log(typeof userID)
+        try {
+            const user = await User.findById(userID)
+
+            if (user) {
+                res.status(200).json(user)
+            } else {
+                throw new BadRequest("User did not found")
+            }
+        } catch (error) {
+            next(error)
+        }
+    },
+    async GetOngoingCourse(req, res, next) {
         const userID = req.user.id
 
         try {
             const user = await User.findById(userID)
 
-            if(user){
-                res.status(200).json(user)
+            let courses = []
+
+            user.courses.forEach((course) => {
+                if (course.progress < 100 || !course.isGraduate) {
+                    courses.push(course)
+                }
+            })
+
+            if (user) {
+                res.status(200).json(courses)
             } else {
-                throw new BadRequest("User did not found")
+                throw new BadRequest("User not found")
+            }
+        } catch (error) {
+            next(error)
+        }
+    },
+    async GetCompletedCourse(req, res, next) {
+        const userID = req.user.id
+
+        try {
+            const user = await User.findById(userID)
+
+            let courses = []
+
+            user.courses.forEach((course) => {
+                if (course.progress == 100 || course.isGraduate) {
+                    courses.push(course)
+                }
+            })
+
+            if (user) {
+                res.status(200).json(courses)
+            } else {
+                throw new BadRequest("User not found")
+            }
+        } catch (error) {
+            next(error)
+        }
+    },
+    async GetAllCourse(req, res, next) {
+        const userID = req.user.id
+
+        try {
+            const user = await User.findById(userID)
+
+            if (user) {
+                res.status(200).json(user.courses)
+            } else {
+                throw new BadRequest("User not found")
             }
         } catch (error) {
             next(error)
--- router/user.js
+++ router/user.js
@@ -1,7 +1,18 @@
 const router = require('express').Router()
 
-const {GetProfile} = require('../handler/user')
+const {
+    GetProfile,
+    GetOngoingCourse,
+    GetCompletedCourse,
+    GetAllCourse
+} = require('../handler/user')
 
 router.get('/', GetProfile)
+
+router.get('/ongoing', GetOngoingCourse)
+
+router.get('/completed', GetCompletedCourse)
+
+router.get('/courses', GetAllCourse)
 
 module.exports = router
